d1ddcf6cc01eb20dfffac45e7e0f4db741c32f03
ExtractionDev <saskiller930@gmail.com>
2026-06-27 20:15:20 -0500

    Create callback.js

---
+++ api/callback.js
@@ -0,0 +1,24 @@
+export default async function handler(req, res) {
+  const { code } = req.query;
+  const response = await fetch('https://api.twitter.com/2/oauth2/token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Basic ' + Buffer.from(
+        process.env.TWITTER_CLIENT_ID + ':' + process.env.TWITTER_CLIENT_SECRET
+      ).toString('base64')
+    },
+    body: new URLSearchParams({
+      code,
+      grant_type: 'authorization_code',
+      redirect_uri: 'https://extraction.online/callback',
+      code_verifier: 'challenge'
+    })
+  });
+  const tokens = await response.json();
+  const userResponse = await fetch('https://api.twitter.com/2/users/me', {
+    headers: { 'Authorization': `Bearer ${tokens.access_token}` }
+  });
+  const { data } = await userResponse.json();
+  res.redirect(`/?username=${data.username}&name=${data.name}`);
+}
